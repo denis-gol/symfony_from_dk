57ed75e000fcf292cabe73778b9d2468d4d596cb
Denis Golitsinskiy <dagol.1980@gmail.com>
2020-05-14 12:28:32 +0300

    - добавлен контроллер для роута tests/N - добавлен шаблон для вывода списка дат

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -29,4 +29,36 @@ class TestController extends AbstractController
         );
     }
 
+    /**
+     * @Route(
+     *     "/tests/{count}",
+     *     name="showDateList",
+     *     methods={"GET"}
+     * )
+     * @param int $count
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function showDateList(int $count)
+    {
+        $date = date('d.m.Y');
+
+        $validator = $this->get('validator');
+        $errors = $validator->validate($count);
+
+        if (count($errors) > 0) {
+            return $this->render('test/errors.twig', array(
+                'errors' => $errors,
+            ));
+        }
+
+        return $this->render(
+            'test/test_n.twig',
+            [
+                'date' => $date,
+                'count' => $count,
+                'errors' => $errors,
+            ]
+        );
+    }
+
 }
