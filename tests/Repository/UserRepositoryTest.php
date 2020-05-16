f233f1d118674f435208e31e684dddff2837a7a6
Denis Golitsinskiy <dagol.1980@gmail.com>
2020-05-16 15:09:51 +0300

    - создана заготовка для теста репозитория User - добавлена заглушка

---
+++ tests/Repository/UserRepositoryTest.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Repository;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class UserRepositoryTest extends WebTestCase
+{
+
+    public function testUpgradePassword()
+    {
+        $this->markTestIncomplete();
+    }
+}
